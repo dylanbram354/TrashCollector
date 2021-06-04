7471b6f03c09167d72a99ffa5f3b8d3de01faeeb
Jeremiah Booth <jeremiah.boothphotog@gmail.com>
2021-06-04 09:26:41 -0700

    updated the Customer model with new attributes

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import DateField
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
@@ -7,3 +8,11 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    pickup_day = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    balance = models.IntegerField()
+    onetime_pickup = models.DateField()
+    suspension_start = models.DateField()
+    suspension_end = models.DateField()
+
